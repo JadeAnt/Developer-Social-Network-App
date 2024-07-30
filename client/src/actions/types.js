35053ef9b4f6a3cd7ba2e9bf58653fda4fd2a4c5
JadeAnt <jadenjantoine@gmail.com>
2024-07-29 21:31:09 -0400

    begun work on react user authentication

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,4 @@
 export const SET_ALERT = 'SET_ALERT'; //since this is going to be used in many different places, can just change it once here
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
